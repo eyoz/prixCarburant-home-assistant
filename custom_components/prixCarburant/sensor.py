636171985b3f31933457d997f1b1de7aed18a425
vingerha <44190435+vingerha@users.noreply.github.com>
2022-03-17 14:28:48 +0100

    integrated prixCarburantClient
    - put in custom_component folder for ease of further local tweaking
    - redirected to instantane'
    - changed xml filename due to fix above

--- custom_components/prixCarburant/sensor.py
+++ custom_components/prixCarburant/sensor.py
@@ -42,7 +42,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
-    from prixCarburantClient.prixCarburantClient import PrixCarburantClient
+    from .prixCarburantClient import PrixCarburantClient
     logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
     logging.info("[prixCarburantLoad] start")
     """Setup the sensor platform."""
