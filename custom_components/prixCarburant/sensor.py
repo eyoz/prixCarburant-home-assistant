eb4883dbb884ff3ab670525a16e71856d091e4dd
vingerha <44190435+vingerha@users.noreply.github.com>
2022-05-23 11:42:09 +0200

    Update sensor.py

--- custom_components/prixCarburant/sensor.py
+++ custom_components/prixCarburant/sensor.py
@@ -104,19 +104,19 @@ class PrixCarburant(Entity):
         self.lastUpdate=self.client.lastUpdate
         self.lastUpdateTime=datetime.now()
         self._unique_id = "PrixCarburant_" + self.station.id
-        logging.warning("[Fueltype (configyaml] " + fuelType)
-        if fuelType = 'gazoil':
+        logging.warning("[Fueltype (configyaml] " + str(fuelType))
+        if str(fuelType) == 'gazoil':
             self._state = self.station.gazoil['valeur']
             logging.warning("[Fueltypeg (configyaml] " + fuelType)    
-        if fuelType = 'E95':
+        if fuelType == 'E95':
             self._state = self.station.e95['valeur']
-        if fuelType = 'E98':
+        if fuelType == 'E98':
             self._state = self.station.e98['valeur']    
-        if fuelType = 'E10':
+        if fuelType == 'E10':
             self._state = self.station.e10['valeur']
-        if fuelType = 'E85':
+        if fuelType == 'E85':
             self._state = self.station.e85['valeur']
-        if fuelType = 'GPL':
+        if fuelType == 'GPL':
             self._state = self.station.gpl['valeur']
         else: 
             self._state = 'n.a.'
